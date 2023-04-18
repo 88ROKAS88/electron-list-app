b65d900d92a8428c411fd649fe80091e368358f1
88ROKAS88 <rokas8misevicius@gmail.com>
2023-04-18 20:31:51 +0300

    bug fix
    2 shortcuts for 1 list bug fix.

--- list app/js/Restrictions.js
+++ list app/js/Restrictions.js
@@ -2,9 +2,18 @@ class Restrictions {
   static listName(name) {
     let flag = true;
     let message = "";
+    // check if field is filled
     if (name.length < 1) {
       flag = false;
       message += " Please enter list name. ";
+    }
+    // check if list name already exist
+    const data = MyNavigation.loadNavigation();
+    for (const element of data) {
+      if (name.toLowerCase() == element["name"].toLowerCase()) {
+        flag = false;
+        message += " " + name + " - list name is already taken. ";
+      }
     }
 
     if (flag) {
@@ -13,7 +22,7 @@ class Restrictions {
       MyNavigation.nav.appendChild(CreateElement.alert(message));
       setTimeout(() => {
         MyNavigation.displayNavigation();
-      }, "2000");
+      }, "3000");
 
       return false;
     }
